1e4f01c6f9075a59a5cab2dadb4093a9a9d07afd
Yonas44 <tesfu.yonas1350@gmail.com>
2023-01-26 21:23:35 +0300

    Create DB from diagram along with tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,59 @@
+
+-- Day-2 Create a schema from a diagram
+
+CREATE DATABASE clinic;
+
+CREATE TABLE medical_histories(
+	id INT GENERATED ALWAYS As IDENTITY,
+	admitted_at TIMESTAMP,
+	patient_id INT,
+	status VARCHAR,
+	PRIMARY KEY(id),
+	CONSTRAINT FK_patient_id FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
+
+
+CREATE TABLE treatments(
+	id INT GENERATED ALWAYS As IDENTITY,
+	type VARCHAR,
+	name VARCHAR,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items(
+	id INT GENERATED ALWAYS As IDENTITY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT,
+	treatment_id INT,
+	PRIMARY KEY(id),
+	CONSTRAINT FK_treatment_id 
+				FOREIGN KEY(treatment_id) 
+					REFERENCES treatments(id),
+	CONSTRAINT	FK_invoice_id 
+				FOREIGN KEY(invoice_id) 
+					REFERENCES invoices(id)
+);
+
+CREATE TABLE invoices(
+	id INT GENERATED ALWAYS As IDENTITY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INT,
+	PRIMARY KEY(id),
+	CONSTRAINT FK_medical_history_id FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE patients(
+	id INT GENERATED ALWAYS As IDENTITY,
+	name VARCHAR,
+	date_of_birth DATE,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_treatment_histories (
+    md_history_id INT REFERENCES medical_histories(id),
+    treatment_id INT REFERENCES treatments(id)
+);
